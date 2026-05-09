336b39dd462ea83737b9145b28288ac662b48193
-YOSEFU- <yosefu1029@gmail.com>
2026-05-09 14:59:14 -0500

    am so stoopid

--- ypods1/pack-info.js
+++ ypods1/pack-info.js
@@ -197,7 +197,7 @@ document.addEventListener('DOMContentLoaded', () => {
         bannerDiv.appendChild(img);
         li.appendChild(bannerDiv);
 
-        if (window.innerWidth > 800) {
+        if (window.innerWidth < 800) {
             img.style.scale = '0.5';
         }
 
